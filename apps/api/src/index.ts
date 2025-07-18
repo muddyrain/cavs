a9d3928bdac87992261fd6e6a3a2301ce977aa76
chenzihan.cavs <chenzihan.cavs@bytedance.com>
2025-07-18 10:36:03 +0800

    feat: 添加api服务项目

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello Koa2 + TypeScript!';
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
