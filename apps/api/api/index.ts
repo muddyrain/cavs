62ae84c3284a1d886d5cf69fa2a14719c33aaf43
muddyrain <qiu3291002845@gmail.com>
2025-07-19 17:36:48 +0800

    feat: 简化api

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -2,13 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const { sql } = require('@vercel/postgres');
 
 const bodyParser = require('body-parser');
 const path = require('path');
-
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
