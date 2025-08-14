e09260feb0393197a7a083e68f44cd0882aecb67
muddyrain <qiu3291002845@gmail.com>
2025-08-14 23:33:26 +0800

    feat(editor): 更新editor编译配置以及schemas节点

--- packages/editor/src/schemas/index.ts
+++ packages/editor/src/schemas/index.ts
@@ -19,13 +19,40 @@ export const paragraph: NodeSpec = {
 		return ["p", { style: `text-align: ${align}` }, 0]
 	}
 }
+
 export const schema: Schema = new Schema({
 	nodes: {
 		doc: { content: "block+" },
 		text: {
 			group: "inline"
 		},
-		paragraph
+		paragraph,
+		heading: {
+			attrs: { level: { default: 1 } },
+			content: "inline*",
+			group: "block",
+			defining: true,
+			parseDOM: [
+				{ tag: "h1", attrs: { level: 1 } },
+				{ tag: "h2", attrs: { level: 2 } },
+				{ tag: "h3", attrs: { level: 3 } },
+				{ tag: "h4", attrs: { level: 4 } },
+				{ tag: "h5", attrs: { level: 5 } },
+				{ tag: "h6", attrs: { level: 6 } }
+			],
+			toDOM(node) {
+				return ["h" + node.attrs.level, 0]
+			}
+		},
+		blockquote: {
+			content: "block+",
+			group: "block",
+			defining: true,
+			parseDOM: [{ tag: "blockquote" }],
+			toDOM() {
+				return ["blockquote", 0]
+			}
+		}
 	},
 	marks: schemaBasic.spec.marks.append({ underline, strikethrough })
 })
