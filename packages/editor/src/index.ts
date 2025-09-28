3bf41a1be34fd7db9735340690fa72f7808424b9
muddyrain <qiu3291002845@gmail.com>
2025-09-28 23:50:00 +0800

    feat(editor): tiptap添加yjs插件

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,7 +1,4 @@
 import "./index.css"
 
-export * from "@/components/ProseMirrorEditor"
 export * from "@/components/TiptapEditor"
-export { useEditor } from "@/hooks/useEditor"
-export * from "@/hooks/useEditorStatus"
 export * from "@/types"
