11d069755338913c60c2fac97323662b5e86c664
muddyrain <qiu3291002845@gmail.com>
2025-10-10 22:46:45 +0800

    feat(editor): 自定义块级规范和行内规范

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,10 @@
-import { useCreateBlockNote } from "@cavs/editor-react"
-import { BlockNoteView } from "@cavs/editor-shadcn"
 import "@cavs/editor-shadcn/style.css"
-import { zh } from "@cavs/editor-core/locales"
+import { DocEditor } from "./pages/Doc/DocEditor"
 
 function App() {
-	const editor = useCreateBlockNote({
-		dictionary: zh
-	})
 	return (
 		<div className="h-screen flex flex-col bg-background">
-			<BlockNoteView editor={editor} />
+			<DocEditor />
 		</div>
 	)
 }
