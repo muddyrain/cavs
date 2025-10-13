d0aa30f9c2d1dafdd03a3724576162aa2c09ff6e
muddyrain <qiu3291002845@gmail.com>
2025-10-13 23:32:40 +0800

    feat(client): react-flow

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,12 @@
 import "@cavs/editor-shadcn/style.css"
 import { DocEditor } from "./pages/Doc/DocEditor"
+import { DocGraph } from "./pages/DocGraph"
 
 function App() {
 	return (
 		<div className="h-screen flex flex-col bg-background">
-			<DocEditor />
+			{/* <DocEditor /> */}
+			<DocGraph />
 		</div>
 	)
 }
