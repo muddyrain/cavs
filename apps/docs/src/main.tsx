714974cb7b9375b3ace330d9c603b6fb9087f29a
muddyrain <qiu3291002845@gmail.com>
2025-12-17 23:11:38 +0800

    feat: 更新docs样式

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -1,7 +1,7 @@
 import { Spreadsheet } from "@cavs/spreadsheet"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
+import "./index.css"
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Spreadsheet />
