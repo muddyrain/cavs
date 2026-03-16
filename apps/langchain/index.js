c0bda18a11de970d8e5890e716dbda3de2468385
muddyrain <qiu3291002845@gmail.com>
2026-03-16 22:03:28 +0800

    feat: 模型接口

--- apps/langchain/index.js
+++ apps/langchain/index.js
@@ -1,16 +1,38 @@
-import { Ollama } from "@langchain/ollama"
+import {
+	ChatPromptTemplate,
+	HumanMessagePromptTemplate,
+	SystemMessagePromptTemplate
+} from "@langchain/core/prompts"
+import { ChatOllama } from "@langchain/ollama"
 
-// 创建 Ollama 模型实例
-const model = new Ollama({
-	model: "llama3"
+const model = new ChatOllama({
+	model: "llama3",
+	temperature: 0.7,
+	stream: true
 })
 
-const prompt = "你是一位中文智能助手，请你用中文回答 AI 对人类文明的影响？"
+// 创建多角色的提示词模板
 
-// 调用模型生成回答
-const res = await model.stream(prompt)
+// 1. 创建系统提示词
+const sysPrompt = SystemMessagePromptTemplate.fromTemplate(
+	"你是一个翻译助理 ,请将用户输入的内容由{input_language}直接翻译为{output_language}."
+)
 
-// 实时读取输出内容
+// 2. 创建用户提示词
+const humanPrompt = HumanMessagePromptTemplate.fromTemplate("{text}")
+
+// 3. 合成提示词
+const chatPrompt = ChatPromptTemplate.fromMessages([sysPrompt, humanPrompt])
+
+// 4. 填充变量
+const messages = await chatPrompt.formatMessages({
+	input_language: "中文",
+	output_language: "英语",
+	text: "我是邱顺蒙，你是谁，我是超级大反派！"
+})
+
+const res = await model.stream(messages)
+
 for await (const chunk of res) {
-	process.stdout.write(chunk)
+	process.stdout.write(chunk.content)
 }
