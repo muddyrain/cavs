6aa064b034d8d67c4d9cbdc2718dd9b6f2fed341
muddyrain <qiu3291002845@gmail.com>
2026-05-01 14:16:22 +0800

    feat: 本地资源loader

--- apps/langchain/index.js
+++ apps/langchain/index.js
@@ -1,39 +1,37 @@
-import { StringOutputParser } from "@langchain/core/output_parsers"
-import { PromptTemplate } from "@langchain/core/prompts"
-import { RunnableLambda, RunnablePassthrough, RunnableSequence } from "@langchain/core/runnables"
-import { ChatOllama } from "@langchain/ollama"
+// import { TextLoader } from "@langchain/classic/document_loaders/fs/text"
+// import { RecursiveCharacterTextSplitter } from "@langchain/classic/text_splitter"
 
-// 记录日志
-const logFun = new RunnablePassthrough({
-	func: async (input) => {
-		console.log("用户提出的问题:", input.query)
-		return input
-	}
+// const loader = new TextLoader("data/long.txt")
+
+// const result = await loader.load()
+
+// const splitter = new RecursiveCharacterTextSplitter({
+// 	chunkSize: 200,
+// 	chunkOverlap: 50
+// })
+
+// const splitResult = await splitter.splitDocuments(result)
+
+// console.log(splitResult)
+
+// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+
+// const loader = new PDFLoader("data/novel.pdf", {
+// 	splitPages: false
+// })
+
+// const result = await loader.load()
+
+// console.log(result)
+
+import { DirectoryLoader } from "@langchain/classic/document_loaders/fs/directory"
+import { TextLoader } from "@langchain/classic/document_loaders/fs/text"
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+
+const loader = new DirectoryLoader("./data", {
+	".pdf": (path) => new PDFLoader(path, { splitPages: false }),
+	".txt": (path) => new TextLoader(path)
 })
 
-// 注入 用户id
-const injector = RunnablePassthrough.assign({
-	userId: async () => "user-123"
-})
-
-// 提示词模板
-const pt = PromptTemplate.fromTemplate(
-	"请使用中文帮用户 {userId} 解释以下概念：{query},回答问题时以亲爱的{userId}称呼他。"
-)
-
-// 模型
-const model = new ChatOllama({ model: "llama3", temperature: 0.7 })
-
-// 输出解析器
-const parser = new StringOutputParser()
-
-// 构建完整的流水线
-const chain = RunnableSequence.from([logFun, injector, pt, model, parser])
-
-const res = await chain.stream({
-	query: "什么是量子计算？"
-})
-
-for await (const chunk of res) {
-	process.stdout.write(chunk)
-}
+const result = await loader.load()
+console.log(result)
