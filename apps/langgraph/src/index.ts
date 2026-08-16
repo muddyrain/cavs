9912e207aeefef94c41ae8261d923f8f83525e0d
muddyrain <qiu3291002845@gmail.com>
2026-08-16 16:25:39 +0800

    feat: 长期记忆

--- apps/langgraph/src/index.ts
+++ apps/langgraph/src/index.ts
@@ -1,93 +1,159 @@
 import "dotenv/config"
-import { HumanMessage } from "@langchain/core/messages"
-import { END, MemorySaver, START, StateGraph } from "@langchain/langgraph"
-
+import { BaseMessage, HumanMessage } from "@langchain/core/messages"
+import { END, InMemoryStore, MemorySaver, START, StateGraph } from "@langchain/langgraph"
+import { ChatOpenAI } from "@langchain/openai"
 import readline from "readline-sync"
-import { SUMMARY_THRESHOLD } from "./config.ts"
+import { z } from "zod"
 import { model } from "./model.ts"
-import { Schema, type TState } from "./state.ts"
-import { summarizeNode } from "./summarizeNode.ts"
 
-// 用户系统
-const users = {
-	aaa: { password: "111", thread_id: "thread-aaa" },
-	bbb: { password: "222", thread_id: "thread-bbb" }
-}
+// 图的状态结构，里面只有一项，消息
+const StateSchema = z.object({
+	messages: z.array(z.custom<BaseMessage>())
+})
 
-// 聊天的节点
-async function chatNode(state: TState): Promise<Partial<TState>> {
-	const res = await model.invoke(state.messages)
+// 根据Schema生成对应的ts类型
+type TState = z.infer<typeof StateSchema>
+
+const store = new InMemoryStore() // 做长期记忆
+const checkpointer = new MemorySaver() // 做短期记忆
+
+// 节点函数 - 聊天
+async function chatNode(state: TState, config: any): Promise<Partial<TState>> {
+	// 1. 从长期记忆里面获取信息
+	// 2. 和大模型进行交流 - 会把长期记忆的信息带过去
+	// 3. 判断新的这一轮会话，有没有信息需要存入到长期记忆里面
+	const userId = config.configurable.userId
+	const namespace = ["user_profile", userId]
+
+	// 取出长期记忆
+	// 记忆里面如果有东西：
+	/**
+   * [
+      {
+        key: "xxxx",  
+        value: {
+          data: "我叫张三。"  // 记忆内容
+        }
+      },
+      {
+        key: "xxxx",  
+        value: {
+          data: "我喜欢编程。"  // 另一条记忆内容
+        }
+      }
+    ]
+   * 
+   */
+
+	const memories = await store.search(namespace, {})
+	console.log("memories>>>", memories)
+	// 取出记忆的内容，组装成一个字符串
+	// "我叫张三。\n 我喜欢编程。"
+
+	const memoryText = memories?.map((m) => m.value.data).join("\n") || ""
+
+	// console.log("memoryText>>>", memoryText);
+
+	// 提示词
+	const systemPrompt = `
+你是一个持续与用户聊天的助手。
+以下是你已知的用户长期信息（如果有）：
+${memoryText || "（暂无）"}
+`
+
+	// 和大模型进行交流
+	const response = await model.invoke([
+		{ role: "system", content: systemPrompt }, // 系统设定
+		...state.messages
+	])
+
+	// 现在我们其实已经有了模型的回复
+	// 先不着急推入到 messages 里面
+	// 先把用户发的消息取出来，判断用户发的消息是否需要存储到长期记忆里面
+	const lastMsg = state.messages.at(-1) // 取出最后一条内容（用户发的）
+	const lastUserMsg = typeof lastMsg?.content === "string" ? lastMsg.content : ""
+
+	// 需要判断是否有存储到长期记忆里面的必要
+	if (shouldSaveToMemory(lastUserMsg)) {
+		// 如果进入此分支，说明要做长期记忆
+		// 需要做一个信息的提取
+		const memory = extractMemory(lastUserMsg)
+		if (memory) {
+			// 提取到信息了，存
+			await store.put(namespace, crypto.randomUUID(), {
+				data: memory
+			})
+			console.log("🧠 [长期记忆已保存]:", memory)
+		}
+	}
 
 	return {
-		messages: [...state.messages, res]
+		messages: [...state.messages, response]
 	}
 }
 
-const checkpointer = new MemorySaver()
+function shouldSaveToMemory(text: string): boolean {
+	// 定义一组关键字，如果用户说的话包含这些关键词，就认为是个人信息
+	const keywords = ["我是", "我叫", "我在", "我的工作", "我喜欢", "记住"]
+	return keywords.some((k) => text.includes(k))
+}
 
-// 图
-const graph = new StateGraph(Schema)
+function extractMemory(text: string): string | null {
+	if (text.includes("我叫")) {
+		return text
+	}
+	if (text.includes("我是")) {
+		return text
+	}
+	if (text.includes("我喜欢")) {
+		return text
+	}
+	if (text.includes("我的工作是")) {
+		return text
+	}
+	return null
+}
+
+const graph = new StateGraph(StateSchema)
 	.addNode("chatNode", chatNode)
-	.addNode("summarizeNode", summarizeNode)
 	.addEdge(START, "chatNode")
-	.addConditionalEdges("chatNode", (state: TState) => {
-		if (state.messages.length > SUMMARY_THRESHOLD) return "summarizeNode"
-		return END
+	.addEdge("chatNode", END)
+	.compile({
+		checkpointer, // 注入短期记忆检查点
+		store // 注入长期记忆存储
 	})
-	.addEdge("summarizeNode", END)
-	.compile({ checkpointer })
 
 async function main() {
+	const config = {
+		configurable: {
+			userId: "bill",
+			thread_id: "bill-chat"
+		}
+	}
+
+	let state: TState = {
+		messages: []
+	}
+
+	console.log("🤖 聊天开始（Ctrl+C 退出）")
+
 	while (true) {
-		console.log("\n--- 登录 ---")
-		const username = readline.question("请输入用户名：")
-		const password = readline.question("请输入密码：", {
-			hideEchoBack: true
-		})
+		// 获取用户在终端的输入
+		const input = readline.question("\n你：")
 
-		const user = users[username]
-		if (!user || user.password !== password) {
-			console.log("登录失败☹️")
-			continue
-		}
+		// 将用户的输入封装成 HumanMessage 对象，加入到本地状态
+		state.messages.push(new HumanMessage(input))
 
-		// 登录成功
-		console.log(`✅ 登录成功，当前用户：${username}`)
-		console.log("输入 /switch 可切换用户\n")
+		const result = await graph.invoke(state, config)
 
-		const config = {
-			configurable: {
-				thread_id: user.thread_id // 这个非常重要，这个保证了不同的用户拥有不同的state
-			}
-		}
+		// console.log("result>>>", result);
 
-		// 处理用户的对话
-		while (true) {
-			const input = readline.question(`${username}>`)
-			if (input === "/switch") {
-				console.log("切换用户中...")
-				break
-			}
+		const aiMsg = result.messages.at(-1)
 
-			// 这里获取到的最新的状态
-			// messages里面是包含了所有的信息的
-			// stateSnapshot ->
-			// { values: { messages: [HumanMessage("hello"), AIMessage("hi")] }, config: { thread_id: "thread-aaa" }, ... }
-			const stateSnapshot = await graph.getState(config)
-			const currentMessages = stateSnapshot.values.messages || []
+		console.log("\nAI:", aiMsg?.content)
 
-			const result = await graph.invoke(
-				{
-					// 之前的状态 + 这一次用户的输入
-					messages: [...currentMessages, new HumanMessage(input)]
-				},
-				config
-			)
-
-			const aiMesg = result.messages.at(-1)
-			console.log(aiMesg?.content)
-		}
+		state = result // 更新本地状态
 	}
 }
 
-await main()
+main()
